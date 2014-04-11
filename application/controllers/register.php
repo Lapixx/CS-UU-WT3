563a018cafc6c4b5b09ddbb08ed7ab80f5a8c27f
Jordi Vermeulen <cloud_strife559@hotmail.com>
2014-04-11 18:30:36 +0200

    YOU SHALL NOT READ!!!!

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -16,13 +16,12 @@ class Register extends CI_Controller {
         $this->form_validation->set_rules('dob', 'Date of birth', 'required|callback_date_valid');
         $this->form_validation->set_rules('description', 'About you', 'required');
         $this->form_validation->set_rules('gender_pref', 'Gender preference', 'required');
-        $this->form_validation->set_rules('brands[]', 'Brands', 'callback_brands_valid');
-        $this->form_validation->set_rules('questions[]', 'Questions', 'callback_questions_valid');
+        $this->form_validation->set_rules('brands', 'Brands', 'callback_brands_valid');
+        $this->form_validation->set_rules('questions', 'Questions', 'callback_questions_valid');
 
         $this->form_validation->set_message('is_unique', 'Already in use.');
         $this->form_validation->set_message('required', 'This field is required.');
 
-        shuffle($this->questions);
         $data = array('brands' => array('Skyrim', 'Kerbal Space Program', 'The Banner Saga', 'Final Fantasy VII'), 'questions' => $this->questions);
         if ($this->form_validation->run()) {
             $email = $this->input->post('email');
@@ -86,6 +85,28 @@ class Register extends CI_Controller {
         return count($questions) == count($this->questions);
     }
 
+    private function buildPersonality() {
+        $pers = array('I' => 50, 'N' => 50, 'T' => 50, 'P' => 50);
+
+        $answers = $this->input->post('questions');
+        foreach ($answers as $i => $ans) {
+            $question = $this->questions[$i];
+            if ($ans == 'a') {
+                $pers[$question[3]] += $question[4];
+            }
+            else if ($ans == 'b') {
+                $pers[$question[3]] -= $question[4];
+            }
+        }
+
+        $pers['I'] = round($pers['I']);
+        $pers['N'] = round($pers['N']);
+        $pers['T'] = round($pers['T']);
+        $pers['P'] = round($pers['P']);
+
+        return $pers;
+    }
+
     private function buildProfile() {
         $profile = array();
 
@@ -99,32 +120,34 @@ class Register extends CI_Controller {
         $profile['min_age'] = $this->input->post('min_age');
         $profile['max_age'] = $this->input->post('max_age');
         $profile['brands'] = implode(',', $this->input->post('brands'));
-        $profile['personality'] = '100,100,100,100';
-        $profile['personality_preference'] = '000,000,000,000';
+
+        $personality = $this->buildPersonality();
+        $profile['personality'] = implode(',', $personality);
+        $profile['personality_preference'] = implode(',', array(100 - $personality['I'], 100 - $personality['N'], 100 - $personality['T'], 100 - $personality['P']));
 
         return $profile;
     }
 
-    // (Question number, Option A, Option B, Option C, Affected parameter, Effect in % x10)
+    // (Question number, Option A, Option B, Option C, Affected parameter, Effect in %)
     private $questions = array(
-            array(1, 'I prefer large groups of people, with a high degree of diversity.', 'I prefer intimate gatherings with only close friends.', 'I am really inbetween.', 'I', '-100'),
-            array(2, 'I act first, then think.', 'I think first, then act.', 'I am really inbetween.', 'I', '-100'),
-            array(3, 'I am easily distracted and am less interested in specific tasks.', 'I can concentrate well and am less interested in the big picture.', 'I am really inbetween.', 'I', '-100'),
-            array(4, 'I am a good speaker and enjoy going out.', 'I am a good listener and am more of a private person.', 'I am really inbetween.', 'I', '-100'),
-            array(5, 'When hosting an event, I am always at the centre of attention.', 'When hosting an event, I work quietly behind the scenes to make sure everything goes smoothly.', 'I am really inbetween.', 'I', '-100'),
-            array(6, 'I prefer concepts and learning based on associations.', 'I prefer observations and learning based on facts.', 'I am really inbetween.', 'N', '125'),
-            array(7, 'I have an active imagination and have a global overview of a project.', 'I am pragmatic and have an detailed knowledge of every step.', 'I am really inbetween.', 'N', '125'),
-            array(8, 'I look to the future.', 'I keep my focus on the present.', 'I am really inbetween.', 'N', '125'),
-            array(9, 'I enjoy the dynamic aspect of relations and tasks.', 'I enjoy the predictability of relations and tasks.', 'I am really inbetween.', 'N', '125'),
-            array(10, 'I completely think my decisions through.', 'I decide based on intuition.', 'I am really inbetween.', 'T', '125'),
-            array(11, 'I work best with a list of pros and cons.', 'I decide based on the consequences for people.', 'I am really inbetween.', 'T', '125'),
-            array(12, 'I am inherently critical.', 'I like to make sure people enjoy themselves.', 'I am really inbetween.', 'T', '125'),
-            array(13, 'I am honest rather than tactical.', 'I am tactical rather than honest.', 'I am really inbetween.', 'T', '125'),
-            array(14, 'I prefer familiar situations.', 'I prefer flexible situations.', 'I am really inbetween.', 'P', '83'),
-            array(15, 'I plan everything, with a "to do" list at hand.', 'I wait until I have several ideas and then decide what to do on the fly.', 'I am really inbetween.', 'P', '83'),
-            array(16, 'I like finishing up projects.', 'I like starting up projects.', 'I am really inbetween.', 'P', '83'),
-            array(17, 'I get stressed by abrupt changes and a lack of planning.', 'I find detailed plans stifling and look forward to change.', 'I am really inbetween.', 'P', '83'),
-            array(18, 'I am more likely to reach a goal.', 'I am more likely to see an opportunity.', 'I am really inbetween.', 'P', '83'),
-            array(19, 'All play and no work causes the project to go unfinished.', 'All work and no play makes you a boring chap.', 'I am really inbetween.', 'P', '83')
+            array('I prefer large groups of people, with a high degree of diversity.', 'I prefer intimate gatherings with only close friends.', 'I am really inbetween.', 'I', '-10'),
+            array('I act first, then think.', 'I think first, then act.', 'I am really inbetween.', 'I', '-10'),
+            array('I am easily distracted and am less interested in specific tasks.', 'I can concentrate well and am less interested in the big picture.', 'I am really inbetween.', 'I', '-10'),
+            array('I am a good speaker and enjoy going out.', 'I am a good listener and am more of a private person.', 'I am really inbetween.', 'I', '-10'),
+            array('When hosting an event, I am always at the centre of attention.', 'When hosting an event, I work quietly behind the scenes to make sure everything goes smoothly.', 'I am really inbetween.', 'I', '-10'),
+            array('I prefer concepts and learning based on associations.', 'I prefer observations and learning based on facts.', 'I am really inbetween.', 'N', '12.5'),
+            array('I have an active imagination and have a global overview of a project.', 'I am pragmatic and have an detailed knowledge of every step.', 'I am really inbetween.', 'N', '12.5'),
+            array('I look to the future.', 'I keep my focus on the present.', 'I am really inbetween.', 'N', '12.5'),
+            array('I enjoy the dynamic aspect of relations and tasks.', 'I enjoy the predictability of relations and tasks.', 'I am really inbetween.', 'N', '12.5'),
+            array('I completely think my decisions through.', 'I decide based on intuition.', 'I am really inbetween.', 'T', '12.5'),
+            array('I work best with a list of pros and cons.', 'I decide based on the consequences for people.', 'I am really inbetween.', 'T', '12.5'),
+            array('I am inherently critical.', 'I like to make sure people enjoy themselves.', 'I am really inbetween.', 'T', '12.5'),
+            array('I am honest rather than tactical.', 'I am tactical rather than honest.', 'I am really inbetween.', 'T', '12.5'),
+            array('I prefer familiar situations.', 'I prefer flexible situations.', 'I am really inbetween.', 'P', '8.333'),
+            array('I plan everything, with a "to do" list at hand.', 'I wait until I have several ideas and then decide what to do on the fly.', 'I am really inbetween.', 'P', '8.333'),
+            array('I like finishing up projects.', 'I like starting up projects.', 'I am really inbetween.', 'P', '8.333'),
+            array('I get stressed by abrupt changes and a lack of planning.', 'I find detailed plans stifling and look forward to change.', 'I am really inbetween.', 'P', '8.333'),
+            array('I am more likely to reach a goal.', 'I am more likely to see an opportunity.', 'I am really inbetween.', 'P', '8.333'),
+            array('All play and no work causes the project to go unfinished.', 'All work and no play makes you a boring chap.', 'I am really inbetween.', 'P', '8.333')
         );
 }
--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -4,7 +4,7 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$data = array('data' => array_merge($this->usermodel->getUserByID(2), $this->usermodel->getProfileByID(2)));
+		$data = array('data' => array_merge($this->usermodel->getUserByID(3), $this->usermodel->getProfileByID(3)));
 		$this->load->view('welcome_message', $data);
 	}
 }

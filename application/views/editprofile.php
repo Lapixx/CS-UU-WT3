3068f7fb451edf02c79b3c52596ca0c8fa354572
Jordi Vermeulen <cloud_strife559@hotmail.com>
2014-04-13 18:02:09 +0200

    More editing

--- application/views/editprofile.php
+++ application/views/editprofile.php
@@ -1,6 +1,18 @@
 <?php
 	echo form_open('editprofile');
 
+	echo form_label('First name', 'first_name');
+	echo form_input(array('name' => 'first_name', 'id' => 'first_name', 'value' => set_value('first_name', $defaults['first_name'])));
+	echo form_error('first_name') . '<br />';
+
+	echo form_label('Last name', 'last_name');
+	echo form_input(array('name' => 'last_name', 'id' => 'last_name', 'value' => set_value('last_name', $defaults['last_name'])));
+	echo form_error('last_name') . '<br />';
+
+	echo form_label('Nickname', 'nickname');
+	echo form_input(array('name' => 'nickname', 'id' => 'nickname', 'value' => set_value('nickname', $defaults['nickname'])));
+	echo form_error('nickname') . '<br />';
+
 	echo form_label('About you', 'description');
 	echo form_textarea(array('name' => 'description', 'id' => 'description', 'value' => set_value('description', $defaults['description']), 'cols' => '50', 'rows' => '10', 'maxlength' => '500'));
 	echo form_error('description') . '<br />';
